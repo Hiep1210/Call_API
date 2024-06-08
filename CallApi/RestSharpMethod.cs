de445cd0854d4ea79e17ac89b8ce61b1f9191e55
Hiep1210 <nguyenhoanghiep1210003@gmail.com>
2024-06-08 21:15:11 +0700

    fix restsharp

--- CallApi/RestSharpMethod.cs
+++ CallApi/RestSharpMethod.cs
@@ -7,55 +7,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace CallApi
 {
     internal class RestSharpMethod
     {
-        private static string link = "http://localhost:5000/api";
-        static RestClientOptions options = new RestClientOptions(link)
-        {
-            Authenticator = new HttpBasicAuthenticator("username", "password")
-        };
-        static RestClient client = new RestClient(options);
+        private string link = "http://localhost:5000/api";
+
         internal async Task ShowList()
         {
-            try { 
-            
-                
-
-                var request = new RestRequest("/Category");
-
-                var timeline = await client.GetAsync<List<Category>>(request);
-                Display(timeline);
+            try
+            {
+                var options = new RestClientOptions(link)
+                {
+                    Authenticator = new HttpBasicAuthenticator("username", "password")
+                };
+                //var client = new RestClient(configureSerialization: s => s.UseNewtonsoftJson());
+                //List<Category> res = await client.GetJsonAsync<List<Category>>(link + "/Category");
+                var client = new RestClient(options, configureSerialization: s => s.UseNewtonsoftJson());
+                List<Category> res = await client.GetJsonAsync<List<Category>>("/Category");
+                Display(res);
             }
             catch (Exception ex)
             {
                 await Console.Out.WriteLineAsync("Exception : " + ex.Message);
             }
         }
-
-        //internal async Task ShowList()
-        //{
-        //    try
-        //    {
-        //        var options = new RestClientOptions(link)
-        //        {
-        //            Authenticator = new HttpBasicAuthenticator("username", "password")
-        //        };
-        //        //var client = new RestClient(configureSerialization: s => s.UseNewtonsoftJson());
-        //        //List<Category> res = await client.GetJsonAsync<List<Category>>(link + "/Category");
-        //        var client = new RestClient(options, configureSerialization: s => s.UseNewtonsoftJson());
-        //        List<Category> res = await client.GetJsonAsync<List<Category>>("/Category");
-        //        Display(res);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        await Console.Out.WriteLineAsync("Exception : " + ex.Message);
-        //    }
-        //}
 
         internal async Task Post(Category cat)
         {
@@ -80,4 +58,3 @@ namespace CallApi
         }
     }
 }
-
