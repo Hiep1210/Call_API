5bb3866044f682fe02746bb3f1cd6cc9b32aefed
Hiep1210 <nguyenhoanghiep1210003@gmail.com>
2024-06-09 23:57:47 +0700

    add newtonsoft serialize for refit

--- CallApi/RefitMethod.cs
+++ CallApi/RefitMethod.cs
@@ -11,13 +11,17 @@ public class RefitMethod
     private JsonSerializer serializer;
     public RefitMethod()
     {
-        CateData = RestService.For<ICategoryData>(link);
+        CateData = RestService.For<ICategoryData>(link, new RefitSettings
+        {
+            ContentSerializer = new NewtonsoftJsonContentSerializer(new JsonSerializerSettings())
+        });
         serializer = new Newtonsoft.Json.JsonSerializer();
     }
     public async Task getAllCategories()
     {
 
         List<Category> Categories =  await CateData.GetCategories();
+        //List<Category> cates = serializer.Deserialize<List<Category>>(new JsonTextReader(new StringReader(strCate)));
         Display(Categories.ToArray());
     }
 
